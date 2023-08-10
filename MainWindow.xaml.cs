0c8ede628e2250be0abd3a9be3837f79d1595199
denis <denispavlenko7610new@gmail.com>
2023-08-10 08:47:12 +0300

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace CurrentTimeApp
+{
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateCurrentTime();
+            UpdateCurrentDate();
+        }
+
+        private void UpdateCurrentTime()
+        {
+            currentTimeText.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void UpdateCurrentDate()
+        {
+            currentDateText.Text = DateTime.Now.ToString("dddd, MMMM d");
+        }
+    }
+}
